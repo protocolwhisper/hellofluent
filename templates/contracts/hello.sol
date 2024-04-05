4de29372f4a437b1d4139658679208f550358d2b
Protocol W <protocolw@Protocols-MacBook-Pro.local>
2024-04-05 14:45:25 -0500

    (fix): js File generation

--- templates/contracts/hello.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-    pragma solidity ^0.8.0;
-
-    contract Hello {
-        function main() public pure returns (string memory) {
-            return "Hello, Solidity!";
-        }
-    }
