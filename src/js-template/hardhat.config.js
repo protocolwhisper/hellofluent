ce77ea94916de3431aa4b36332b63316176ef21c
protocolwhisper <chrismata10@gmail.com>
2024-05-17 12:33:10 -0500

    (feature): Updated Rpc and Rust template

--- src/js-template/hardhat.config.js
+++ src/js-template/hardhat.config.js
@@ -6,7 +6,7 @@ require("@nomiclabs/hardhat-vyper");
     module.exports = {
       networks: {
         fluent_devnet1: {
-          url: 'https://rpc.dev1.fluentlabs.xyz/', 
+          url: 'https://rpc.dev.thefluent.xyz/', 
           chainId: 1337, 
           accounts : [
             `0x${"ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"}` ], // Replace with the private key of the deploying account
--- src/ts-template/hardhat.config.ts
+++ src/ts-template/hardhat.config.ts
@@ -5,7 +5,7 @@ import "@nomiclabs/hardhat-vyper";
 const config: HardhatUserConfig = {
   networks: {
     fluent_devnet1: {
-      url: 'https://rpc.dev1.fluentlabs.xyz/',
+      url: 'https://rpc.dev.thefluent.xyz/',
       chainId: 1337,
       accounts: [
         `0x${"ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"}`
